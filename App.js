00295fd1267f932da3d7523e52ed269ffe8b2eb1
Jack Hedaya <jackehedaya@gmail.com>
2018-11-27 18:44:09 -0500

    refactoring

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
-import style from "./styles/App"
+import styles from "./styles/App"
 
-import CreateAccount from "./components/CreateAccount"
+import CreateAccount from "./components/major/CreateAccount"
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={style.container}>
+      <View style={styles.container}>
         <CreateAccount />
       </View>
     );
