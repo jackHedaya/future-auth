f9b8fbdf94e575acf32ae53f7227b2642ea781bf
Jack Hedaya <jackehedaya@gmail.com>
2018-11-27 18:44:01 -0500

    reorganized

---
+++ components/major/CreateAccount.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { View } from "react-native";
+import FieldItem from "../minor/FieldItem";
+import MainButton from "../minor/MainButton";
+
+export default class CreateAccount extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: ""
+    };
+
+    this.verifyAndContinue = this.verifyAndContinue.bind(this);
+  }
+
+  verifyAndContinue() {
+    // if (/^\S+@\S+\.\S+$/.test(this.state.email)) 
+    //   console.log("Continue")
+    // else
+
+    
+  }
+
+  render() {
+    return (
+      <View>
+        <FieldItem
+          title={"Email"}
+          value={this.state.email}
+          onChangeText={t => {
+            if (t[t.length - 1] !== " ") this.setState({ email: t });
+          }}
+          keyboardType={"email-address"}
+        />
+        <MainButton title={"Continue"} onPress={this.verifyAndContinue} />
+      </View>
+    );
+  }
+}
