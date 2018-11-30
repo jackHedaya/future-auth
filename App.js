4351f8601276e6f83b9dd20df6feaadc39ed08f5
Jack Hedaya <jackehedaya@gmail.com>
2018-11-29 21:46:40 -0500

    implemented React Router

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { View } from 'react-native';
-import styles from "./styles/App"
+import React from "react";
+import { View } from "react-native";
+import { NativeRouter, Route } from "react-router-native";
 
-import CreateAccount from "./components/major/CreateAccount"
+import styles from "./styles/App";
+
+import Opening from "./components/major/Opening";
+import CreateAccount from "./components/major/CreateAccount";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <CreateAccount />
-      </View>
+      <NativeRouter>
+        <View style={styles.container}>
+          <Route exact path="/" component={Opening} />
+          <Route exact path="/signup" component={CreateAccount} />
+        </View>
+      </NativeRouter>
     );
   }
 }
-
