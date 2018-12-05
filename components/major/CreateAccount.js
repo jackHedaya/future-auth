a5be0338a75a1769d4aadfa413983623186a30a0
Jack Hedaya <jackehedaya@gmail.com>
2018-12-05 10:19:07 -0500

    continue actually works, goBack also works

--- components/major/CreateAccount.js
+++ components/major/CreateAccount.js
@@ -1,28 +1,33 @@
 import React from "react";
 import { View } from "react-native";
+import { withRouter } from "react-router-native"
+
 import FieldItem from "../minor/FieldItem";
 import MainButton from "../minor/MainButton";
+import BackButton from "../minor/BackButton";
 
-export default class CreateAccount extends React.Component {
+import appStyle from "../../styles/App";
+
+class CreateAccount extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: ""
+      email: "",
+      shouldShowPassword: false
     };
 
     this.verifyAndContinue = this.verifyAndContinue.bind(this);
   }
 
   verifyAndContinue() {
-    // if (/^\S+@\S+\.\S+$/.test(this.state.email))
-    //   console.log("Continue")
-    // else
+    if (/^\S+@\S+\.\S+$/.test(this.state.email)) this.setState({ shouldShowPassword: true });
   }
 
   render() {
     return (
-      <>
+      <View style={appStyle.screen}>
+        <BackButton onPress={() => this.props.history.pop} />
         <FieldItem
           title={"Email"}
           value={this.state.email}
@@ -31,8 +36,11 @@ export default class CreateAccount extends React.Component {
           }}
           keyboardType={"email-address"}
         />
+        {this.state.shouldShowPassword ? <FieldItem title={"Password"} /> : null}
         <MainButton onPress={this.verifyAndContinue}>Continue</MainButton>
-      </>
+      </View>
     );
   }
 }
+
+export default withRouter(CreateAccount)
